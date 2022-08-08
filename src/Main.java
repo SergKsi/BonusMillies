00a700d85be4a7fb69fc55f4709e0b177471db23
Sergey Kosovichev <sergksi@mail.ru>
2022-08-09 01:12:39 +0300

    Modify main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 49;        // стоимость билета
+        int countRubBonusMiles = 20; // количество рублей для одной бонусной милли
+        int countBonusMilies = 0;   // количество бонусных миль
+
+        // формула расчета :
+        // за каждые 20 рублей, потраченные на билет, начисляется 1 миля.
+        countBonusMilies = (priceTicket * 1) / countRubBonusMiles;
+
+        System.out.println("Количество бонусных миль = " + countBonusMilies);
+    }
 }
